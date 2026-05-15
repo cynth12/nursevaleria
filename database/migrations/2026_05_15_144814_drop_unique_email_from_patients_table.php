4b8f06d4bd419ff50208afb7e905ce56e0ccc666
cynth12 <gonzalez.cynth12@gmail.com>
2026-05-15 09:49:12 -0500

    email unic

---
+++ database/migrations/2026_05_15_144814_drop_unique_email_from_patients_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+{
+    Schema::table('patients', function (Blueprint $table) {
+        $table->dropUnique('patients_email_unique');
+    });
+}
+
+public function down()
+{
+    Schema::table('patients', function (Blueprint $table) {
+        $table->unique('email');
+    });
+}
+};
