90f215039beb313beac70d87beaf7b7129881bef
cynth12 <gonzalez.cynth12@gmail.com>
2026-06-18 13:38:15 -0500

    buscador

--- app/Http/Controllers/PacientesController.php
+++ app/Http/Controllers/PacientesController.php
@@ -9,45 +9,40 @@ use App\Models\Consentimiento;
 class PacientesController extends Controller
 {
     public function index(Request $request)
-{
-    $search = $request->search;
+    {
+        $search = $request->search;
 
-    $patients = Patient::when($search, function ($query) use ($search) {
+        $patients = Patient::when($search, function ($query) use ($search) {
+            $query->whereRaw("CONCAT(TRIM(name), ' ', TRIM(last_name)) LIKE ?", ["%{$search}%"]);
+        })
+            ->orderBy('registration_date', 'desc')
+            ->paginate(10);
 
-        $query->whereRaw("CONCAT(name, ' ', last_name) LIKE ?", ["%{$search}%"]);
+        return view('pacientes.index', compact('patients'));
+    }
 
-    })
-    ->orderBy('registration_date', 'desc')
-    ->paginate(10);
+    public function mes($mes)
+    {
+        $patients = Patient::whereYear('registration_date', 2026)->whereMonth('registration_date', $mes)->orderBy('registration_date', 'desc')->paginate(10);
 
-    return view('pacientes.index', compact('patients'));
-}
+        $nombreMes = [
+            1 => 'Enero',
+            2 => 'Febrero',
+            3 => 'Marzo',
+            4 => 'Abril',
+            5 => 'Mayo',
+            6 => 'Junio',
+            7 => 'Julio',
+            8 => 'Agosto',
+            9 => 'Septiembre',
+            10 => 'Octubre',
+            11 => 'Noviembre',
+            12 => 'Diciembre',
+        ];
 
-public function mes($mes)
-{
-    $patients = Patient::whereYear('registration_date', 2026)
-    ->whereMonth('registration_date', $mes)
-    ->orderBy('registration_date', 'desc')
-    ->paginate(10);
-
-    $nombreMes = [
-        1 => 'Enero',
-        2 => 'Febrero',
-        3 => 'Marzo',
-        4 => 'Abril',
-        5 => 'Mayo',
-        6 => 'Junio',
-        7 => 'Julio',
-        8 => 'Agosto',
-        9 => 'Septiembre',
-        10 => 'Octubre',
-        11 => 'Noviembre',
-        12 => 'Diciembre'
-    ];
-
-    return view('pacientes.index', [
-        'patients' => $patients,
-        'titulo' => 'Pacientes registrados en ' . $nombreMes[$mes]
-    ]);
-}
+        return view('pacientes.index', [
+            'patients' => $patients,
+            'titulo' => 'Pacientes registrados en ' . $nombreMes[$mes],
+        ]);
+    }
 }
